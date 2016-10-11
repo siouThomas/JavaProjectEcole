89f004c76bcd342c55fdca4360bd6a6c441ace9c
Thomas <thomas.gorand@gmail.com>
2016-10-11 12:07:44 +0200

    Question Politicus Systeme

---
+++ app/src/main/java/com/app/politcus/com/politicus/questions/QuestionType.java
@@ -0,0 +1,35 @@
+package com.app.politcus.com.politicus.questions;
+
+/**
+ * Created by Thomas on 11/10/2016.
+ */
+
+
+/**
+ *
+ * Enum permettant de stocker les différents états possible des question
+ *
+ * L'état est stoker sous forme d'un int
+ *
+ * Chaque type de question possible est à ajouter dans l'enum
+ *
+ */
+public enum QuestionType {
+    None(0),
+
+    TrueFalse(1),
+
+    OneAnswer(2),
+
+    MultiAnswer(3);
+
+    private int value;
+
+    QuestionType(int value) {
+        this.value = value;
+    }
+
+    public int get() {
+        return value;
+    }
+}
