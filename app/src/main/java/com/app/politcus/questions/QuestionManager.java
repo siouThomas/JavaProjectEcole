d81d044fe7d0d061102948a36579595d8740d0e1
Gilles RIVIERE <griviere07@gmail.com>
2016-10-13 14:55:48 +0200

    refactored question manager, added mecanism to filter already used questions for a certain time

--- app/src/main/java/com/app/politcus/questions/QuestionManager.java
+++ app/src/main/java/com/app/politcus/questions/QuestionManager.java
@@ -3,37 +3,31 @@ package com.app.politcus.questions;
 import java.util.ArrayList;
 import java.util.Random;
 
-/**
- * Created by Thomas on 11/10/2016.
- */
-
-
-public class QuestionManager
-{
+public class QuestionManager {
 
     private static volatile QuestionManager instance;
 
     final private ArrayList<QuestionQuizz> questionsQuizz = new ArrayList<QuestionQuizz>();
     final private ArrayList<QuestionTest> questionsTest = new ArrayList<QuestionTest>();
 
-    //final private ArrayList<Question> questionsStocks = new ArrayList<Question>();
+    // List the questions already requested. Use this to avoid sending the same question too many
+    // times to the user.
+    final private ArrayList<Integer> questionsTestServed = new ArrayList<Integer>();
+    final private ArrayList<Integer> questionsQuizzServed = new ArrayList<Integer>();
 
-    private QuestionManager()
-    {
+
+    private QuestionManager() {
     }
 
     /**
      * permet de récuperer l'instance du singleton
+     *
      * @return
      */
-    public final static QuestionManager getInstance()
-    {
-        if (instance == null)
-        {
-            synchronized(QuestionManager.class)
-            {
-                if (instance == null)
-                {
+    public final static QuestionManager getInstance() {
+        if (instance == null) {
+            synchronized (QuestionManager.class) {
+                if (instance == null) {
                     instance = new QuestionManager();
                 }
             }
@@ -44,37 +38,93 @@ public class QuestionManager
     /**
      * en attendant ANTOINE LIEBERTTTla liason bdd en entre en dur les question
      */
-    void init()
-    {
-        Question tmp = new Question();
+    void init() {
+        QuestionQuizz tmp = new QuestionQuizz(true);
+        QuestionTest tmp2 = new QuestionTest();
 
-        for(int i = 0; i < 5 ; i ++)
-        {
-            tmp.setTitle("Question example"+ i );
-            tmp.setType(QuestionType.TrueFalse);
+        for (int i = 0; i < 5; i++) {
+            tmp.setTitle("Question example" + i);
+            tmp.setId(i);
+            this.questionsQuizz.add(tmp);
 
-            try
-            {
-                tmp.addAnswers(Answer.True);
-
-            } catch (Exception e)
-            {
-                //Todo gerer les erreurs
-                System.out.println("error dans les question");
-                e.printStackTrace();
-            }
-
-
-            this.questions.add(tmp);
+            tmp2.setTitle("Question test example " + i);
+            tmp2.setId(i);
+            questionsTest.add(tmp2);
         }
-
-        questionsStocks.clear();
+        questionsTestServed.clear();
+        questionsQuizzServed.clear();
     }
 
-    /**
-     * add question in storage question and in stock question
-     * @param newQuestions
-     */
+    public QuestionTest getRandomQuestionTest(){
+        // reset counter if every questions were already used once.
+        if(questionsTestServed.size() == questionsTest.size()){
+            questionsTestServed.clear();
+        }
+
+        Random rand = new Random();
+        int id = -1;
+
+        do{
+            id = rand.nextInt(questionsTest.size());
+        } while (questionsTestServed.contains(id));
+
+        return getQuestionTestWithId(id);
+    }
+
+    public QuestionTest getQuestionTestWithId(int id){
+
+        for(QuestionTest q : questionsTest){
+            if(q.getId() == id){
+                questionsTestServed.add(id);
+                return q;
+            }
+        }
+
+        if(questionsTest.isEmpty()){
+            QuestionTest q = new QuestionTest();
+            q.setTitle("Error : Empty database. Please complain to Mr Liebert");
+            return q;
+        }
+        return questionsTest.get(0);
+    }
+
+    public QuestionQuizz getQuestionQuizz(){
+        // reset counter if every questions were already used once.
+        if(questionsQuizzServed.size() == questionsQuizz.size()){
+            questionsQuizzServed.clear();
+        }
+
+        Random rand = new Random();
+        int id = -1;
+
+        do{
+            id = rand.nextInt(questionsTest.size());
+        } while (questionsTestServed.contains(id));
+
+        return getQuestionQuizzWithId(id);
+    }
+
+    public QuestionQuizz getQuestionQuizzWithId(int id) {
+
+        for (QuestionQuizz q : questionsQuizz) {
+            if (q.getId() == id) {
+                questionsTestServed.add(id);
+                return q;
+            }
+        }
+
+        if (questionsQuizz.isEmpty()) {
+            QuestionQuizz q = new QuestionQuizz();
+            q.setTitle("Error : Empty database. Please complain to Mr Liebert");
+            return q;
+        }
+        return questionsQuizz.get(0);
+    }
+}
+
+
+/* Cache manamgent should go to the DAO
+
     void add(Question newQuestions)
     {
             questions.add(newQuestions);
@@ -82,11 +132,6 @@ public class QuestionManager
     }
 
 
-    /**
-     * add some question
-     *
-     * @param newQuestions
-     */
     void adds(ArrayList<Question> newQuestions)
     {
         for (Question q : newQuestions) {
@@ -96,9 +141,7 @@ public class QuestionManager
         }
     }
 
-    /**
-     * initialisation Question Stocks
-     */
+
     private void initStock()
     {
         for (Question q : questions) {
@@ -108,11 +151,6 @@ public class QuestionManager
         }
     }
 
-    /**
-     * get Question in stock. If there are no questions in stock the systeme add the same questions.
-     *
-     * @return Question
-     */
     private Question getQuestionStock()
     {
         if(questionsStocks.size() <= 0) {
@@ -129,29 +167,8 @@ public class QuestionManager
         return  questionTmp;
     }
 
-    /**
-     *
-     * Get several Questions in stocks.
-     *
-     * @param numberQuestion
-     * @return
-     * @throws Exception
-     */
-    public ArrayList<Question> get(int numberQuestion) throws Exception
-    {
-
-        if(this.questions.size() < numberQuestion)
-            throw new Exception();
-
-        ArrayList<Question> questionsTmp = new ArrayList<Question>();
-
-        for ( int i = 0; i < numberQuestion; i ++ ) {
 
 
-            questionsTmp.add(getQuestionStock());
-        }
-
-        return questionsTmp;
-
-    }
 }
+
+*/
